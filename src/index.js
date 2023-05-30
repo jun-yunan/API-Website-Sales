0464b8c6dbe3654067cbd98c90ad01b592295cf9
jun-yunan <junyunan341695@gmail.com>
2023-05-30 17:02:21 +0700

    #2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const route = require('./routes');
 const handlebars = require('express-handlebars').engine;
 
 const app = express();
-const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -23,6 +22,7 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
 });
